77c16540f0e447578d25d0c96682216d92f9fe4b
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-05-15 19:18:47 -0400

    commit tree 868ca5807244e19a76a3047101e0876076c446cf

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+
+
+const driver = { key: "value" };
+
+const output = Object.assign({}, obj);
+
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign ({}, obj, { [key]: value })
+
+}
+
+
+
+
+
+function DestructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+
+
+
+
+function deleteFromDriverByKey(obj, key) {
+    output = Object.assign ({}, obj)
+    delete output[key]
+    return output
+
+}
+
+
+
+
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
